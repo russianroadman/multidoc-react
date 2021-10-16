af8ab052fb64cc89e52b2437dd1c60a159d2ac38
russianroadman <realgar2@gmail.com>
2021-10-17 00:23:01 +0800

    websocket

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './editor.css'
-import {BrowserRouter, Route} from "react-router-dom";
-import Main from "./Main";
+import {BrowserRouter, Route} from 'react-router-dom';
+import Main from './Main';
+import { w3cwebsocket as Ws } from 'websocket'
+import {useEffect} from "react";
+
+const client = new Ws('ws://localhost:8080')
 
 function App() {
+
+    useEffect(() => {
+        client.onopen = () => {
+            console.log('client connected')
+        }
+        if (client.readyState === 1){
+            client.send(
+                JSON.stringify({
+                    type: 'message',
+                    msg: 'goddammit'
+                })
+            )
+        }
+    }, [])
+
     return (
         <BrowserRouter>
             <>
